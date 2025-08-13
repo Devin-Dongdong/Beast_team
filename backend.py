dd8a884d94ba1583150643e45d9cd4ee9ce0b467
Shafer, Ethan H MAJ <ethan.shafer+usma@westpoint.edu>
2025-08-13 10:41:25 -0400

    chore: fix black formatting issues

--- backend.py
+++ backend.py
@@ -2,16 +2,17 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 # Define some variables.
-ip = '0.0.0.0'
+ip = "0.0.0.0"
 port = 8000
 server_address = (ip, port)
 
-# Define and init a variable called "api" using the classes we imported. 
+# Define and init a variable called "api" using the classes we imported.
 api = HTTPServer(
-    server_address=server_address,
-    RequestHandlerClass=SimpleHTTPRequestHandler
+    server_address=server_address, RequestHandlerClass=SimpleHTTPRequestHandler
 )
 
 # Start the HTTP server.
-print(f"Listening for HTTP requests on http://{api.server_address[0] if ip != '0.0.0.0' else 'localhost'}:{api.server_port} ...")
+print(
+    f"Listening for HTTP requests on http://{api.server_address[0] if ip != '0.0.0.0' else 'localhost'}:{api.server_port} ..."
+)
 api.serve_forever()
